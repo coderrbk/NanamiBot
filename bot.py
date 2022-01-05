cc3c5482d82a9634529934e09d9618282cfe10c0
coderrbk <384195127@qq.com>
2022-01-05 17:28:04 +0800

    improvement:为了发布v0.1.1的改动

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 
 nonebot.init()
 nonebot.load_plugins("src/plugins")
-# nonebot.load_plugins("src/plugins/foo")
+nonebot.load_plugins("src/plugins/foo")
 # 加载插件目录
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)
